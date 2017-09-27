f5694f54d9a26f85705fed41f86c6411964276c7
southwolf <wp.southwolf@gmail.com>
2017-09-27 10:43:08 +0800

    render templates

--- Gemfile
+++ Gemfile
@@ -4,3 +4,6 @@ ruby '2.4.2'
 
 gem 'bundler', '~> 1.0'
 gem 'em-midori', '~> 0.4.0', require: 'midori'
+
+# render templates
+gem 'tilt', '~> 2.0.0'
--- app.rb
+++ app.rb
@@ -1,9 +1,26 @@
 require 'bundler'
 Bundler.require
+require 'erb'
+require 'tilt'
+
+Midori::Configure.before = proc do
+  Dir[File.dirname(__FILE__) + '/models/*.rb'].each { |file| require file }
+end
 
 class App < Midori::API
   get '/' do
-    'Hello Midori' 
+    me = Person.new
+    me.name = 'Midori'
+    me.age = '1 year'
+    me.gender = 'girl'
+    me.photo = 'https://github.com/heckpsi-lab/em-midori/raw/master/.resources/midori_logo.png'
+
+    template = Tilt.new('views/hello.html.erb')
+    Midori::Response.new(
+      status: 200,
+      header: { 'Content-Type': 'text/html' },
+      body: template.render(me)
+    )
   end
 end
 
